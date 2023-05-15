2674101a96543f128fc70d784f9cf6e1b45b245b
krisnasirait <krisnasirait48@gmail.com>
2023-05-15 15:00:17 +0700

    added function to go to poke detail on clicked list item

--- app/src/main/java/com/krisna/pokemondex/MainActivity.kt
+++ app/src/main/java/com/krisna/pokemondex/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.MaterialTheme
@@ -18,6 +19,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.krisna.pokemondex.model.Pokemon
 import com.krisna.pokemondex.model.PokemonData
 import com.krisna.pokemondex.ui.theme.PokemonDexTheme
@@ -32,9 +37,23 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    PokemonList(pokemons = PokemonData.pokemons, onPokemonClick = {
-                        // TODO: Implement this to navigate to the Pokemon details page
-                    })
+                    val navController = rememberNavController()
+                    NavHost(navController = navController, startDestination = "pokemon_list") {
+                        composable("pokemon_list") {
+                            PokemonList(pokemons = PokemonData.pokemons, onPokemonClick = { pokemon ->
+                                navController.navigate("pokemon_detail/${pokemon.id}")
+                            })
+                        }
+                        composable("pokemon_detail/{pokemonId}") { backStackEntry ->
+                            val pokemonId = backStackEntry.arguments?.getString("pokemonId")
+                            val pokemon = PokemonData.pokemons.find { it.id == pokemonId }
+                            if (pokemon != null) {
+                                PokemonDetail(pokemon)
+                            } else {
+                                Text("Pokemon not found")
+                            }
+                        }
+                    }
                 }
             }
         }
@@ -53,12 +72,24 @@ fun PokemonList(pokemons: List<Pokemon>, onPokemonClick: (Pokemon) -> Unit) {
 
 @Composable
 fun PokemonListItem(pokemon: Pokemon, onClick: () -> Unit) {
-    Row(modifier = Modifier.clickable(onClick = onClick)) {
+    Row(modifier = Modifier
+        .fillMaxWidth()
+        .clickable(onClick = onClick)) {
         Image(painter = painterResource(pokemon.image), contentDescription = pokemon.name)
         Column(modifier = Modifier.padding(start = 8.dp)) {
             Text(text = pokemon.name)
             Text(text = pokemon.id)
         }
+    }
+}
+
+@Composable
+fun PokemonDetail(pokemon: Pokemon) {
+    Column(modifier = Modifier.fillMaxSize()) {
+        Image(painter = painterResource(pokemon.image), contentDescription = pokemon.name)
+        Text(text = pokemon.name, fontSize = 30.sp)
+        Text(text = pokemon.id, fontSize = 20.sp)
+        Text(text = pokemon.description, fontSize = 16.sp)
     }
 }
 
