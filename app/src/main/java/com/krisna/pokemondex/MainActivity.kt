beb5d72ce7d625800884372511f8ed311066d0b5
krisnasirait <krisnasirait48@gmail.com>
2023-05-17 15:05:37 +0700

    change content description name for AboutScreen to about_page

--- app/src/main/java/com/krisna/pokemondex/MainActivity.kt
+++ app/src/main/java/com/krisna/pokemondex/MainActivity.kt
@@ -67,8 +67,8 @@ class MainActivity : ComponentActivity() {
                                     TopAppBar(
                                         title = { Text("Pokemon List") },
                                         actions = {
-                                            IconButton(onClick = { navController.navigate("about") }) {
-                                                Icon(imageVector = Icons.Default.AccountBox, contentDescription = "About")
+                                            IconButton(onClick = { navController.navigate("about_page") }) {
+                                                Icon(imageVector = Icons.Default.AccountBox, contentDescription = "about_page")
                                             }
                                         }
                                     )
@@ -94,7 +94,7 @@ class MainActivity : ComponentActivity() {
                                 Text("Pokemon not found")
                             }
                         }
-                        composable(route = "about") {
+                        composable(route = "about_page") {
                             AboutScreen(navController = navController)
                         }
                     }
